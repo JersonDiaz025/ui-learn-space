41a8a52aa9b507b7fd7868b78fe4d1e5af96e676
Jerson Diaz <thejer19@gmail.com>
2024-08-27 00:49:32 -0400

    Create project

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,12 @@
 import Title from "./Title";
+import Header from "./Header";
 
 const Dashboard = () => {
   return (
     <div>
+      <div className="content-header">
+        <Header/>
+      </div>
       <Title title="Inicio" />
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+
+const Header = () => {
+  return (
+    <div className="header">
+      {/*<div>
+        <button>Profile</button>
+      </div>*/}
+    </div>
+  );
+}
+
+export default Header;
